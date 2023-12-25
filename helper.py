f72d6f77a0936753689e17f472f1f15c9e8f71b2
Sourav Kumar <95288159+singhsourav0@users.noreply.github.com>
2023-12-26 00:08:26 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -79,17 +79,18 @@ def most_common_words(selected_user,df):
     most_common_df = pd.DataFrame(Counter(words).most_common(20))
     return most_common_df
 
-def emoji_helper(selected_user,df):
+def emoji_helper(selected_user, df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
 
     emojis = []
     for message in df['message']:
-        emojis.extend([c for c in message if c in emoji.UNICODE_EMOJI['en']])
+        emojis.extend([c for c in message if c in emoji.UNICODE_EMOJI.get('en', emoji.UNICODE_EMOJI['default'])])
 
     emoji_df = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))))
 
     return emoji_df
+
 
 def monthly_timeline(selected_user,df):
 
