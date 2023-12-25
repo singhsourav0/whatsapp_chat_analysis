6c9ecf96e5edb622fd772d41667bc06fea60ecd0
Sourav Kumar <95288159+singhsourav0@users.noreply.github.com>
2023-12-26 01:02:18 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -82,11 +82,6 @@ def most_common_words(selected_user,df):
 def emoji_helper(selected_user,df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
-
-    emojis = []
-    for message in df['message']:
-       # emojis.extend([c for c in message if c in emoji.UNICODE_EMOJI['en']])
-
     emoji_df = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))))
 
     return emoji_df
